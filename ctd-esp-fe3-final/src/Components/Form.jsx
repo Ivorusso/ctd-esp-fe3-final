870a1e365b795f4fae5e08c906fe33f3d2abcf43
Ivorusso <ivanrodriguezrusso@gmail.com>
2022-12-09 15:27:50 -0300

    form validado

--- ctd-esp-fe3-final/src/Components/Form.jsx
+++ ctd-esp-fe3-final/src/Components/Form.jsx
@@ -5,22 +5,51 @@ import { useState } from "react";
 const Form = () => {
   const [user, setUser] = useState({
     nombre: "",
+    email: ""
   })
-const form = document.querySelector("#form")
+  const [errors, setErrors] = useState({
+    nombre: false,
+    email: false
+  })
+  const form = document.querySelector("#form")
+  const validarNombre = (e) =>{
+    const nameRegex = /^[a-zA-Z0-9]+$/
+    const {value} = e.target
+    if(value.length < 5 || !nameRegex.test(value)){
+        setErrors({...errors, nombre: true})
+    }else{
+        setErrors({...errors, nombre: false})
+    }
+    setUser({...user, nombre: value})
+  }
+  const validarEmail = (e) =>{
+    const emailRegex =  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    const {value} = e.target
+    if( !emailRegex.test(value) ){
+      setErrors({...errors, email: true})
+    }else{
+        setErrors({...errors, email: false})
+    }
+    setUser({...user, email: value})
+  }
 
-const handlesubmit = (e) =>{
-  e.preventDefault()
-  setUser({...user, register: true})
-  form.reset
-}
+  const handlesubmit = (e) =>{
+    e.preventDefault()
+    if(!errors.nombre && !errors.email){
+      setUser({...user, register: true})
+      form.reset
+    }
+  }
 
   return (
     <div>
       <form id="form" onSubmit={handlesubmit}>
           <label> Nombre completo</label>
-          <input type="text" required onChange={(e) => setUser({...user, nombre: e.target.value})}/>
+          <input type="text" required onChange={validarNombre}/>
+          {errors.nombre && <span className="error"> El nombre debe tener mas de 5 caracteres y no tener caracteres especiales</span>}
           <label> Email</label>
-          <input type="email" />
+          <input type="email" required onChange={validarEmail}/>
+          {errors.email && <span className="error">El email no es valido</span>}
           <button type="submit">Enviar</button>
       </form>
 
